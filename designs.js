0d76d46891a158c6b59eb6e33d6ee9b787a4319b
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-01-22 20:35:44 -0500

    Added some javascript and added jquery to html

--- designs.js
+++ designs.js
@@ -1,10 +1,22 @@
 // Select color input
+let colorInput = $("#colorPicker");
 // Select size input
-
+let heightInput = $("#input_height"),
+  widthInput = $("#input_width");
+let color, height, width;
+const canvas = $("#pixel_canvas");
 // When size is submitted by the user, call makeGrid()
 
+colorInput.on('change', function() {
+  color = colorInput.val();
+});
+heightInput.on('change', function() {
+  height = heightInput.val();
+});
+widthInput.on('change', function() {
+  width = widthInput.val();
+});
+
 function makeGrid() {
-
-// Your code goes here!
-
+  
 }
