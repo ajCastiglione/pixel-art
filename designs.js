351a63e6c0e6e3bffda84b3dbaf1c3e94aea4ff3
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-01-22 21:05:10 -0500

    Made some progress with the JS

--- designs.js
+++ designs.js
@@ -1,22 +1,49 @@
-// Select color input
-let colorInput = $("#colorPicker");
-// Select size input
-let heightInput = $("#input_height"),
-  widthInput = $("#input_width");
-let color, height, width;
-const canvas = $("#pixel_canvas");
-// When size is submitted by the user, call makeGrid()
+$(function() {
+  // Select color input
+  let colorInput = $("#colorPicker"),
+    submit = $('#submitSize'),
+    emptyGrid = $("#clearGrid");
+  // Select size input
+  let heightInput = $("#input_height"),
+    widthInput = $("#input_width");
+  let color, height, width;
+  const canvas = $("#pixel_canvas");
+  emptyGrid.hide();
+  // When size is submitted by the user, call makeGrid()
+  height = heightInput.val(); width = widthInput.val();
 
-colorInput.on('change', function() {
-  color = colorInput.val();
-});
-heightInput.on('change', function() {
-  height = heightInput.val();
-});
-widthInput.on('change', function() {
-  width = widthInput.val();
-});
+  colorInput.on('change', function() {
+    color = Number(colorInput.val());
+  });
+  heightInput.on('change keypress', function() {
+    height = Number(heightInput.val());
+  });
+  widthInput.on('change keypress', function() {
+    width = Number(widthInput.val());
+  });
 
-function makeGrid() {
-  
-}
+  function makeGrid() {
+    let tr = $("<tr></tr>"),
+      td = $("<td></td>");
+
+    for (var i = 0; i < height; i++) {
+      canvas.append(tr);
+    }
+    for (var w = 0; w < width; w++) {
+      canvas.find('tr').append(td);
+    }
+  }
+
+  submit.click(function() {
+    makeGrid();
+    $(this).hide();
+    emptyGrid.show();
+  });
+
+  emptyGrid.click(function() {
+    canvas.empty();
+    $(this).hide();
+    submit.show();
+  });
+
+});
